673630e74cb01f58c90d40eeef58e8d105d0d437
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-30 11:58:46 +0300

    adm-set-menu

---
+++ tests/test-order_test-case.py
@@ -0,0 +1,8 @@
+class update_order(test_setup_testcase.AuthTest):
+    def test_get_all_menus(self):
+		""" Test API can get all menus."""
+		
+	def test_update_a_menu(self):
+		""" Test API can update a menu. """
+		
+
