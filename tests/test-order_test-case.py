d9e081dddacd259387354288096f9fc88cf9de4a
Joseph-Odhiambo <josephkdo@gmail.com>
2020-09-30 13:58:27 +0300

    structure

--- tests/test-order_test-case.py
+++ tests/test-order_test-case.py
@@ -13,15 +13,10 @@ def test_get_all_menus(self):
 
 	def test_update_a_menu(self):
 		""" Test API can update a menu. """
-		self.menu = {
-			"name": "KFC Special",
-			"description": "Finger lickin chicken"
-		}
+		self.menu = {"name": "KFC Special", "description": "Finger lickin chicken"}
+			
 		response = self.app.post("/api/v1/menus", data = self.menu)
 		self.assertEqual(response.status_code, 201)
-		self.menu = {
-			"name": "KFC Special 3",
-			"description": "Just Eat Finger lickin chicken..."
-		}
+		self.menu = { "name": "KFC Special 3", "description": "Just Eat Finger lickin chicken..."}
 		response = self.app.put("/api/v1/menus/1", data = self.menu)
 		self.assertEqual(response.status_code, 201)
